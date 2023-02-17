53ce14e94cf8160470f0de479c204b50e62d1f14
Shanmugagouthaman <65980015+SG9822@users.noreply.github.com>
2023-02-17 12:22:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# -------------------- EXTRA HARD PROJECT ---------------------- #
+
+# reader = csv.reader(data)
+# list_data = list(reader)
+# print(list_data)
+
+import pandas
+import smtplib
+import random
+import datetime as dt
+import os
+
+data = pandas.read_csv("birthdays.csv")
+name = data["name"].tolist()
+email = data["email"].tolist()
+year = data["year"].tolist()
+month = data["month"].tolist()
+day = data["day"].tolist()
+
+list_dir = os.listdir("letter_templates")
+
+with open(f"letter_templates/{random.choice(list_dir)}") as file:
+    files = file.read()
+
+my_email = "sgkkr0598@gmail.com"
+password = "zkqpifzdxrsfsvla"
+
+now = dt.datetime.now()
+date = now.day
+months = now.month
+time = now.time()
+print(time)
+
+if months in month and date in day:
+    date_index = day.index(date)
+    name_replaced_file = files.replace("[NAME]", name[date_index])
+    print(name_replaced_file)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as content:
+        content.starttls()
+        content.login(user=my_email, password=password)
+        content.sendmail(
+            from_addr=my_email,
+            to_addrs=email[date_index],
+            msg=f"Subject:Birthday Wishes\n\n{name_replaced_file}"
+        )
